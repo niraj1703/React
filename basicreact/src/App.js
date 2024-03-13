b8f19d0e0307511666982d268680eefa9b4823bb
Niraj Kumar <sharmaniraj763220@gmail.com>
2024-03-13 19:10:54 +0530

    commit second

---
+++ basicreact/src/App.js
@@ -0,0 +1,12 @@
+import Chai from "./Chai.js";
+
+function App() {
+  return (
+  <>
+  <Chai></Chai>
+   
+    </>
+  );
+}
+
+export default App;
---
+++ basicreact/src/Chai.js
@@ -0,0 +1,6 @@
+function Chai(){
+    return(
+        <h2>chai is react</h2>
+    );
+}
+export default Chai;
